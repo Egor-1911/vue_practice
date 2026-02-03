50112bf07cfa41bec7ab6b0037e6f065d6b28bae
gr434_mueva <egormyrt21@gmail.com>
2026-02-03 17:08:40 +0700

    commit components

--- js/main.js
+++ js/main.js
@@ -1,27 +1,75 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 8
-            }
-        ],
-        cart: 0,
+
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of Stock</p>
+<!--            <ul>-->
+<!--                <li v-for="detail in details">{{ detail }}</li>-->
+<!--            </ul>-->
+            
+            <p>Shipping: {{ shipping }}</p>
+            
+            <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+            ></div>
+            <div class="cart">
+            <p>Cart({{ cart }})</p>
+            </div>
+
+            <button
+                v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+            >
+            Add to cart
+            </button>
+            
+        </div>
+
+        
+    </div>
+ `,
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        }
     },
     methods: {
         addToCart() {
@@ -30,7 +78,7 @@ let app = new Vue({
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-        },
+        }
     },
     computed: {
         title() {
@@ -42,15 +90,42 @@ let app = new Vue({
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
         },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' on Sale!'
-            }
-            else {
-                return this.brand + ' ' + this.product + ' Not on Sale!';
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
             }
         }
 
-
-    },
+    }
 })
+
+Vue.component('product-details', {
+    template: `
+    <ul>
+         <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+ `,
+    data() {
+        return {
+            details: ['80% cotton', '20% polyester', 'Gender-neutral']
+        }
+    },
+    methods: {
+        // тут будут методы
+    },
+    computed: {
+        // тут будут вычисляемые свойства
+    }
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
+})
+
+
+
