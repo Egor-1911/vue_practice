f46427ff2935cfe1a826216d8af53a5a359fbe7e
gr434_mueva <egormyrt21@gmail.com>
2026-02-03 15:29:54 +0700

    commit calculated properties

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
 let app = new Vue({
     el: '#app',
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-
-        removeFromCart() {
-            this.cart -= 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-
-    },
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: true,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 8
             }
         ],
-
         cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' on Sale!'
+            }
+            else {
+                return this.brand + ' ' + this.product + ' Not on Sale!';
+            }
+        }
 
-    }
+
+    },
 })
-
